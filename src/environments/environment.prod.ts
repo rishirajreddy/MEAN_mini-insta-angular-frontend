4afe3ad9825fbebdb4d65a8e20234904c82080c1
rishirajreddy <rishirajreddy.07@gmail.com>
2022-09-05 22:59:09 +0530

    aws-s3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   url: "http://meannodeinfinitygrambackend-env.eba-snrz4ntw.us-west-2.elasticbeanstalk.com/api/user"
-  // url: "http://localhost:5000/api/user"
+  // url: "http://localhost:3000/api/user"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  url: "https://ancient-eyrie-80987.herokuapp.com/api/user"
-  // url: "http://localhost:3000/api/user"
+  // url: "https://ancient-eyrie-80987.herokuapp.com/api/user"
+  url: "http://localhost:3000/api/user"
 };
 
 /*
